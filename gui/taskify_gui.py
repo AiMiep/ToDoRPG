43bf591c3e9ac12ea2c631f0642acde3771c4427
aimi.do <aimi.do@burkert.com>
2025-01-17 11:20:58 +0100

    Moved graphic/statistic to taskify menu

--- gui/taskify_gui.py
+++ gui/taskify_gui.py
@@ -1,7 +1,25 @@
 from nicegui import ui
 
+from tasks import get_task_status_counts
+
+user_id = 1
+
 def show_main_menu():
+
     ui.label('Taskify').style('text-align: center; width: 100%; font-size: 32px; font-weight: bold;')
+
+    ui.highchart({
+        'title': {'text': 'Status-Statisik'},
+        'chart': {'type': 'bar'},
+        'xAxis': {'categories': ['Erstellt', 'In Bearbeitung', 'Beendet']},
+        'yAxis': {'title': {'text': 'Anzahl der Aufgaben'}},
+        'series': [
+            {'name': 'Aufgaben',
+             'data': [get_task_status_counts(user_id)['Erstellt'], get_task_status_counts(user_id)['In Bearbeitung'],
+                      get_task_status_counts(user_id)['Beendet']]},
+        ],
+    }).classes('w-full h-60')
+
     ui.button('Neue Aufgabe erstellen',
               on_click=lambda: ui.run_javascript("window.location.href = '/create_task';")).classes('w-full')
     ui.button('Aufgaben anzeigen',
